97c6bf19bc6ec7c694ba132852e716b06cd8ba58
balajis732005 <balajis07032005@gmail.com>
2024-08-22 22:20:31 +0530

    Prefix_To_Postfix_conversion

--- Dsa_Striver_Course/src/STACK_QUEUE/EXPRESSION/Prefix_To_Postfix.java
+++ Dsa_Striver_Course/src/STACK_QUEUE/EXPRESSION/Prefix_To_Postfix.java
@@ -5,7 +5,8 @@ import java.util.Stack;
 
 /*
 POSTFIX TO INFIX CONVERSION:
-1)If ch==Operator -> take last two peeks(p1,p2) and popout p1,p2 and push new string ch+p2+p1 to stack
+NOTE :" star from end of the expression
+1)If ch==Operator -> take last two peeks(p1,p2) and popout p1,p2 and push new string p1+p2+ch to stack
 2)If ch==Operand -> push to stack
 
 TIME COMPLEXITY : O(N) + O(N)
@@ -13,21 +14,21 @@ SPACE COMPLEXITY : O(N)
 
  */
 
-public class Postfix_To_Prefix {
+public class Prefix_To_Postfix {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter the PostFix Expression : ");
-        String postFixExpression = in.next();
-        System.out.println("POSTFIX EXPRESSION FROM INFIX EXPRESSIONS : " +postFixToPrefix(postFixExpression));
+        String preFixExpression = in.next();
+        System.out.println("POSTFIX EXPRESSION FROM INFIX EXPRESSIONS : " +preFixToPostfix(preFixExpression));
     }
-    static String postFixToPrefix(String postFixExpression){
+    static String preFixToPostfix(String preFixExpression){
         Stack<String> stack = new Stack<>();
-        for(int i=0;i<postFixExpression.length();i++){
-            char ch = postFixExpression.charAt(i);
+        for(int i=preFixExpression.length()-1;i>=0;i--){
+            char ch=preFixExpression.charAt(i);
             if(ch=='^' || ch=='*' || ch=='/' || ch=='+' || ch=='-') {
                 String p1=stack.pop();
                 String p2=stack.pop();
-                stack.push(ch+p2+p1);
+                stack.push(p1+p2+ch);
             }else{
                 stack.push(ch+"");
             }
